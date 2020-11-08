8691bc39d3e6c90c80409d14b76001e44433b7f5
DanyGordon <52357936+DanyGordon@users.noreply.github.com>
2020-11-08 22:55:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -93,6 +93,23 @@
             }
         })
     }
+    
+    $('document').ready(function() {
+        $('#uptotop').click(function(){
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
+        });
+    })
+        
+    $(window).scroll(function(){
+        if(window.pageYOffset > 300) {
+            $('#uptotop').show();
+        } else {
+            $('#uptotop').hide();
+        }
+    })
 
     carouselInit();
     buttonInit();
